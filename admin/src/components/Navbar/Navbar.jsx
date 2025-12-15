edaed66e9b55a1213ae2e2e68d2fb62c7ff0c5a8
Ananya$ <way2ananya29@gmail.com>
2025-12-15 19:43:35 +0530

    Integrated new spin wheel feature and updated cart UI

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,9 @@ import { assets } from '../../assets/assets'
 const Navbar = () => {
   return (
     <div className='navbar'>
+      
       <img className='logo' src={assets.logo} alt="" />
+      <h1>ADMIN PANEL</h1>
       <img className='profile' src={assets.profile_image} alt="" />
     </div>
   )
